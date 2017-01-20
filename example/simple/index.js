172841d9b67f477351717b676b9da6bfba693646
skysunshine99 <skysunshine99@126.com>
2017-01-20 11:30:03 +0800

    first commit

---
+++ example/simple/index.js
@@ -0,0 +1,22 @@
+/**
+ * @author rubyisapm
+ */
+import Vue from 'vue';
+import Page from '../../index.js';
+Vue.component(Page.name,Page);
+new Vue({
+    el: '#page',
+    data: {
+        currPage:1,
+        pageLen:20,
+        totalNum:1000,
+        form:true
+    },
+    methods:{
+        changePage(page){
+            this.currPage=page;
+            console.log(page)
+        }
+    }
+
+});
